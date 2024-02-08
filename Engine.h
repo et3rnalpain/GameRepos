34df8d2007d9df9e255631a8431aed8658f5e4d3
et3rnalpain <gjobovi4@mail.ru>
2024-02-08 22:58:04 +0300

    Добавьте файлы проекта.

---
+++ Engine.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include <ctime>
+#include <algorithm>
+//1234
+class Map
+{
+private:
+	int id;
+	double x, y, h = 500, w = 500;
+	sf::Color color;
+public:
+	sf::RectangleShape rect;
+	Map();
+	int getId();
+	void setId(int id);
+	void setXY(int x_, int y_);
+};
+
+
+class Player
+{
+protected:
+	double x, y, ms, health, damage;
+	bool invis;
+public:
+	Player();
+	virtual void attack();
+	virtual void move();
+	virtual void checkPosition();
+};
+
+class PlayerUsual : public Player
+{
+public:
+	void move() override;
+};
+
+class PlayerInvisible : public Player
+{
+
+};
+
+class PlayerBoss : public Player
+{
+
+};
+
+class PlayerSnake : public Player
+{
+
+};
+
+class Game
+{
+private:
+	Player* player;
+	sf::RenderWindow window;
+	Map* maps = new Map[4];
+public:
+	void StartGameCycle();
+};
---
+++ GameRepos.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Engine.h"
+using namespace sf;
+
+int main()
+{
+	srand(time(NULL));
+	Game game;
+	game.StartGameCycle();
+	return 0;
+	//prikol
+}
